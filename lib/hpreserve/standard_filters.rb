430046f4a06eaa6f709f8f963efb2747d59d32e7
Matt Lyon <matt@ameratsu-2.local>
2008-03-23 14:13:50 -0700

    remove upcase, downcase, inactive filters, replace with capitalize

--- lib/hpreserve/standard_filters.rb
+++ lib/hpreserve/standard_filters.rb
@@ -1,20 +1,11 @@
 module Hpreserve
   module StandardFilters
   
-    def upcase(node)
-      node.inner_html = node.inner_text.upcase
+    def capitalize(node)
+      node.inner_html = node.inner_text.capitalize
       node
     end
     
-    def downcase(node)
-      node.inner_html = node.inner_text.downcase
-      node
-    end
-    
-    def formatdate(node, *args)
-      
-    end
-  
   end
 end
 
--- spec/standard_filters_spec.rb
+++ spec/standard_filters_spec.rb
@@ -5,9 +5,9 @@ describe Hpreserve::StandardFilters do
   describe "upcase" do
     before { @f = Hpreserve::Filters.create }
     
-    it "upcases the text of the node" do
+    it "capitalizes the text of the node" do
       @doc = Hpricot("<span>foo</span>")
-      @f.upcase(@doc.at('span')).inner_text.should == 'FOO'
+      @f.capitalize(@doc.at('span')).inner_text.should == 'Foo'
     end
     
   end
